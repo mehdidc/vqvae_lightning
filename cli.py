8ecf01071e1288fc4b721716622cdb34f233ce0c
Mehdi Cherti <mehdicherti@gmail.com>
2020-06-04 07:07:04 +0200

    first

---
+++ cli.py
@@ -0,0 +1,137 @@
+import yaml
+import os
+from functools import partial
+from clize import run
+from argparse import Namespace
+import numpy as np
+from imageio import imsave
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+import torch.nn.functional as F
+import torch.optim.lr_scheduler as lr_scheduler
+import torchvision
+
+from data import Shuffle
+from data import load_dataset
+from data import SubSet
+from model import VQAE
+
+import pytorch_lightning as pl
+from pytorch_lightning.core import LightningModule
+
+
+class Model(LightningModule):
+    
+    def __init__(self, hparams):
+        super().__init__()
+        self.hparams = hparams
+        self.dataset = self.load_dataset(hparams)
+        self.model = self.build_model(hparams)
+
+    def load_dataset(self, hparams):
+        dataset = load_dataset(
+            hparams.dataset_folder, 
+            image_size=hparams.image_size, 
+            nb_channels=hparams.nb_channels
+        )
+        if hparams.nb_examples is not None:
+            dataset = Shuffle(dataset)
+            dataset = SubSet(dataset, hparams.nb_examples)
+        return dataset
+
+    def forward(self, x):
+        return self.model(x)
+
+    def build_model(self, hparams):
+        return VQAE(
+            num_hiddens=hparams.num_hiddens,
+            num_residual_layers=hparams.num_residual_layers,
+            num_residual_hiddens=hparams.num_residual_hiddens,
+            num_embeddings=hparams.num_embeddings,
+            embedding_dim=hparams.embedding_dim,
+            commitment_cost=hparams.commitment_cost,
+            decay=hparams.decay,
+            nb_channels=hparams.nb_channels,
+            nb_blocks=int(np.log2(hparams.image_size//hparams.stride)),
+        )
+    
+    def training_step(self, batch, batch_idx):
+        self.save_grids("train")
+        X, _ = batch
+        commit_loss, XR, perplexity = self.model(X)
+        recons_loss = F.mse_loss(X, XR)
+        loss = recons_loss + commit_loss
+        output = OrderedDict({"loss": loss, "log": {"loss": loss},})
+        return output
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=self.hparams.batch_size,
+            shuffle=True,
+            num_workers=self.hparams.num_workers,
+        )
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(
+            self.parameters(),
+            lr=self.hparams.lr,
+            weight_decay=self.hparams.weight_decay,
+        )
+        scheduler = lr_scheduler.ExponentialLR(
+            optimizer, gamma=self.hparams.scheduler_gamma
+        )
+        return [optimizer], [scheduler]
+    
+    def on_epoch_end(self):
+        self.save_grids("train")
+        self.save_grids("valid")
+    
+    def save_grids(self, split):
+        if split == "train":
+            loader = self.train_dataloader()
+        elif split == "valid":
+            loader = self.valid_dataloader()
+        else:
+            raise ValueError(split)
+
+        X, _ = next(iter(loader))
+        X = X.to(self.device)
+        commit_loss, XR, perplexity = self.model(X)
+        X = X.data.cpu()
+        XR = XR.data.cpu()
+        
+        X_grid = torchvision.utils.make_grid(X)
+        XR_grid = torchvision.utils.make_grid(XR)
+        self.logger.experiment.add_image('inputs', X_grid, self.current_epoch)
+        self.logger.experiment.add_image('reconstructions', XR_grid, self.current_epoch)
+
+        X_grid = X_grid.permute(1,2,0).numpy()
+        XR_grid = XR_grid.permute(1,2,0).numpy()
+        grid = np.concatenate((X_grid, XR_grid), axis=1)
+        grid = (grid*255).astype("uint8")
+        imsave(os.path.join(self.hparams.folder, f"{split}_rec.png"), grid)
+
+
+def train(hparams_path):
+    hparams = load_hparams(hparams_path)
+    os.makedirs(hparams.folder, exist_ok=True)
+    model = Model(hparams)
+    trainer = pl.Trainer(
+        default_root=hparams.folder,
+        max_epochs=hparams.epochs,
+        show_progress_bar=False,
+        gpus=hparams.gpus,
+    )
+    trainer.fit(model)
+
+def load_hparams(path):
+    return Namespace(**yaml.load(open(path).read()))
+
+if __name__ == "__main__":
+    run([train])
---
+++ data.py
@@ -0,0 +1,149 @@
+import random
+import os
+import numpy as np
+from skimage.transform import resize
+from glob import glob
+
+import torch.nn as nn
+import torch
+from torch.utils.data import Dataset, TensorDataset
+from torchvision.datasets import ImageFolder, MNIST
+import torchvision.transforms as transforms
+
+from torchvision.datasets.folder import default_loader
+
+
+class CacheDataset:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.cache = {}
+
+    def __getitem__(self, idx):
+        if idx in self.cache:
+            return self.cache[idx]
+        else:
+            point = self.dataset[idx]
+            self.cache[idx] = point
+            return point
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+def load_dataset(
+    dataset_name,
+    split="train",
+    image_size=32,
+    crop_size=None,
+    random_crop_size=None,
+    cls=ImageFolder,
+    nb_channels=3,
+    invert=False,
+):
+    if dataset_name == "mnist":
+        dataset = MNIST(
+            root=("mnist"),
+            download=True,
+            transform=transforms.Compose(
+                [transforms.Resize((image_size, image_size)), transforms.ToTensor()]
+            ),
+        )
+        return dataset
+    elif "quickdraw" in dataset_name:
+        dataset = QuickDrawDataset(dataset_name)
+        return dataset
+    else:
+        tfs = []
+        if crop_size is not None:
+            tfs.append(transforms.CenterCrop(crop_size))
+        tfs.append(transforms.Resize((image_size, image_size)))
+        if random_crop_size is not None:
+            tfs.append(transforms.RandomCrop(random_crop_size))
+        tfs.append(transforms.ToTensor())
+        if nb_channels == 1:
+            tfs.append(Gray())
+        if invert:
+            tfs.append(Invert())
+        dataset = cls(root=dataset_name, transform=transforms.Compose(tfs))
+        return dataset
+
+
+class QuickDrawDataset:
+    def __init__(self, path):
+        self.data = np.load(path)
+
+    def __getitem__(self, i):
+        x = self.data[i]
+        x = x.reshape((28, 28))
+        x = resize(x, (32, 32), preserve_range=True)
+        x = x / 255.0
+        x = torch.from_numpy(x)
+        x = x.unsqueeze(0)
+        x = x.float()
+        y = 0
+        return x, y
+
+    def __len__(self):
+        return len(self.data)
+
+
+class Invert:
+    def __call__(self, x):
+        return 1 - x
+
+
+class Gray:
+    def __call__(self, x):
+        return x[0:1]
+
+
+
+class SubSet:
+    def __init__(self, dataset, nb):
+        self.dataset = dataset
+        self.nb = nb
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
+
+    def __len__(self):
+        return self.nb
+
+
+class Shuffle:
+    def __init__(self, dataset, seed=42):
+        self.dataset = dataset
+        if hasattr(dataset, "classes"):
+            self.classes = dataset.classes
+        rng = np.random.RandomState(seed)
+        inds = np.arange(len(dataset))
+        rng.shuffle(inds)
+        self.inds = inds
+
+    def __getitem__(self, ind):
+        return self.dataset[self.inds[ind]]
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+class DatasetWithIndices:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, idx):
+        return (idx,) + self.dataset[idx]
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+class ImageFolderWithPath(ImageFolder):
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return sample, target, path
