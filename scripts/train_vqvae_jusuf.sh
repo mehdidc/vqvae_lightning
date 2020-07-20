c940f9e63e8bfbd90bac608eb28c65c43d9a7e09
Mehdi Cherti <m.cherti@fz-juelich.de>
2020-07-20 15:15:56 +0200

    minor

---
+++ scripts/train_vqvae_jusuf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128
+#SBATCH --output=out
+#SBATCH --error=err
+#SBATCH --time=02:00:00
+#SBATCH --partition=gpus
+#SBATCH --gres=gpu:1
+source ~/pyenv
+ml purge
+ml Stages/Devel-2019a
+ml GCC/8.3.0
+ml ParaStationMPI/5.4.4-1
+ml CUDA/10.1.105
+ml NCCL/2.4.6-1-CUDA-10.1.105
+ml cuDNN/7.5.1.10-CUDA-10.1.105
+export NCCL_DEBUG=INFO
+export NCCL_IB_CUDA_SUPPORT=0
+export NCCL_IB_DISABLE=1
+rm -fr results/srgan/vqvae
+srun python -u cli.py train-vqvae configs/vqvae.yaml
